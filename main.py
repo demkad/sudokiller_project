31b0c40802f4de4b6b15ade1a26018cefc637b3d
Yavuzakdeniz <yavuzz.ak@gmail.com>
2024-03-17 03:56:31 +0100

    Herseyi functie yaptimmmm

--- main.py
+++ main.py
@@ -93,14 +93,31 @@ def control_cel(num,cel,rij,kolom,kamer):
     else: 
         return True
 
+set_sudoku = {}
 def oplosser(sudoku):
+    # Aanmaak van een niewe dictionary, value wordt hier een lege set. In deze sets komen de nummers in dat door de programma ingevuld is.
+    
+    for key in sudoku:
+        set_sudoku[key] = set()
+    # Cellen worden vanaf nu een voor een gecontrolleerd
+    prev_waarde = ""
+    prev_cel = "A1"
     for cel,waarde in sudoku.items():
         if waarde == "":
             for num in range(1,10):
                 num = str(num)
-                if control_cel(num,cel,func_rij(sudoku),func_kolom(sudoku),func_kamer(sudoku)): # Hier wordt gecontroleerd of de nummer aanwezig is in de rij,kolom en kamer
+                # Hieronder wordt gecontroleerd of de nummer aanwezig is in de rij,kolom en kamer, indien niet wordt de nummer opgeslaan in de cel.
+                if control_cel(num,cel,func_rij(sudoku),func_kolom(sudoku),func_kamer(sudoku)) and num not in set_sudoku[cel]: 
                     sudoku[cel] = num
                     break # Als de cel ingevuld is stopt de 2de loop, de programma gaat wel verder om de volgende cel te controleren
+        if sudoku[cel] == "":
+            set_sudoku[prev_cel].add(prev_waarde)
+            sudoku[prev_cel] = ""
+            return False
+        prev_cel = cel
+        prev_waarde = num
+                    
+    
                 
                     
 
