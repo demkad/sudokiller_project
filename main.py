823ecf0910f646d551654b0a1e8047d546f85f57
Yavuzakdeniz <yavuzz.ak@gmail.com>
2024-03-18 19:13:24 +0100

    son hali

--- main.py
+++ main.py
@@ -102,21 +102,26 @@ def oplosser(sudoku):
     # Cellen worden vanaf nu een voor een gecontrolleerd
     prev_waarde = ""
     prev_cel = "A1"
-    for cel,waarde in sudoku.items():
-        if waarde == "":
-            for num in range(1,10):
-                num = str(num)
-                # Hieronder wordt gecontroleerd of de nummer aanwezig is in de rij,kolom en kamer, indien niet wordt de nummer opgeslaan in de cel.
-                if control_cel(num,cel,func_rij(sudoku),func_kolom(sudoku),func_kamer(sudoku)) and num not in set_sudoku[cel]: 
-                    sudoku[cel] = num
-                    break # Als de cel ingevuld is stopt de 2de loop, de programma gaat wel verder om de volgende cel te controleren
-        if sudoku[cel] == "":
-            set_sudoku[prev_cel].add(prev_waarde)
-            sudoku[prev_cel] = ""
-            return False
-        prev_cel = cel
-        prev_waarde = num
-                    
+    ingevulde_cellen = []
+    for i in range(100):
+        for cel,waarde in sudoku.items():
+            if waarde == "":
+                for num in range(1,10):
+                    num = str(num)
+                    # Hieronder wordt gecontroleerd of de nummer aanwezig is in de rij,kolom en kamer, indien niet wordt de nummer opgeslaan in de cel.
+                    if control_cel(num,cel,func_rij(sudoku),func_kolom(sudoku),func_kamer(sudoku)) and num not in set_sudoku[cel]: 
+                        sudoku[cel] = num
+                        ingevulde_cellen.append(cel)
+                        break # Als de cel ingevuld is stopt de 2de loop, de programma gaat wel verder om de volgende cel te controleren
+            if sudoku[cel] == "":
+                set_sudoku[prev_cel].add(prev_waarde)
+                sudoku[prev_cel] = ""
+                #for cellen in ingevulde_cellen:
+                #   sudoku[cellen] = ""
+                break
+            prev_cel = cel
+            prev_waarde = num
+    print(set_sudoku)
     
                 
                     
