554560fa476ec681538618f437c19b3fccd66f75
demkad <me@demirtaskadir.com>
2024-03-27 18:59:41 +0100

    temizlik.exe

--- django_web/sudoku/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.index,name="index"),
-    path("index_2",views.index_2,name="index2"),
-    path('<int:category_id>', views.getSudokuByCategoryId, name="sudoku_by_category_id"),
-    path('<str:category>', views.getSudokuByCategory, name='sudoku_by_category'),
-]
